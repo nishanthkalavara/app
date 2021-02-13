bf104042796f75b953771575617201f0082cf5ee
nishanthkalavara <27063516+nishanthkalavara@users.noreply.github.com>
2021-02-13 23:30:46 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:8081"
     "http://127.0.0.1:5500",
+    "https://youthful-feynman-0144cf.netlify.app",
 ]
 
 app.add_middleware(
