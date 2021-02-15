e6833490a196d4a916dbcd37bfdd2013edc71f78
nishanthkalavara <27063516+nishanthkalavara@users.noreply.github.com>
2021-02-15 22:11:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ origins = [
     "http://localhost:8080",
     "http://localhost:8081",
     "http://127.0.0.1:5500",
-    "http://18.212.22.42/frontend",
+    "http://18.212.22.42:8080",
 ]
 
 app.add_middleware(
